9e97b0f2af93888e95c726e4bd3e020c0284cd93
Abbas Zaidan <62467881+zaidanabbas@users.noreply.github.com>
2022-05-28 17:03:21 +0300

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "110px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+console.log("AZaidan Server running v25.4")
